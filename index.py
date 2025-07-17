e6e4b21c9588ffe92a2115afcad7ba7e2b33a428
Amitabh Jain <amijain12@publicisgroupe.net>
2025-07-17 13:30:00 +0530

    changes for reading flags

--- index.py
+++ index.py
@@ -1,9 +1,24 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+import ldclient
+from ldclient.config import Config
+from ldclient.context import Context
+
+load_dotenv()
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+sdk_key = os.getenv("LAUNCHDARKLY_SDK_KEY")
+ldclient.set_config(Config(sdk_key))
 
-if __name__ == '__main__':
+
+@app.route("/")
+def home():
+    context = Context.builder("amitabh123").kind("user").name("amitabh").build()
+    is_active = ldclient.get().variation("Is_Active", context, False)
+
+    return "The feature is ACTIVE 🎉" if is_active else "The feature is INACTIVE 🚫"
+
+if __name__ == "__main__":
     app.run(debug=True)
